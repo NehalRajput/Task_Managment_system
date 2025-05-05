1b465f10e7451b4986598281b917c70d298e88fb
Nehal rajput <nehalrajput1710@Nehals-MacBook-Air.local>
2025-05-05 11:18:14 +0530

    add guard concept for the manage the role

--- my-laravel-project/config/auth.php
+++ my-laravel-project/config/auth.php
@@ -11,12 +11,21 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'admin' => [
+            'driver' =>'session',
+            'provider' => 'admins',
+        ],
     ],
 
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => env('AUTH_MODEL', App\Models\User::class),
+        ],
+
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => env('AUTH_ADMIN_MODEL', App\Models\User::class),
         ],
     ],
 
